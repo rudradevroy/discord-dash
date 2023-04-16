31253967448ba6d421cd672a6f567b5de8444405
Rudradev Roy <rudradev.roy@gmail.com>
2023-04-16 19:20:26 +0530

    added mongoose and moved /lib to root

---
+++ lib/dbConnect.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const { MONGODB_URI, MONGODB_DB } = process.env;
+
+if (!MONGODB_URI) throw new Error('MONGODB_URI not defined');
+if (!MONGODB_DB) throw new Error('MONGODB_DB not defined');
+
+async function dbConnect() {
+  // check if we have a connection to the database or if it's currently
+  // connecting or disconnecting (readyState 1, 2 and 3)
+  if (mongoose.connection.readyState >= 1) {
+    console.log("DB connected");
+    return;
+  }
+  mongoose.connection.on("connected", () => {
+    console.log("connected to mongo db");
+  });
+  // mongoose.connection.on("error", (err) => {
+  //   console.log(`db connection problem`, err.message);
+  // });
+  return mongoose.connect(MONGODB_URI as string, {});
+}
+
+export default dbConnect;
